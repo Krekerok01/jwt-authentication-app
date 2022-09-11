cc53a6646f89c5dfc3b739e8fb1c6fb032128fa8
Varvara Mamatsiuk <matyush2001@gmail.com>
2022-09-11 21:57:28 +0300

    Refactoring changes in the UserServiceImpl class

--- src/main/java/com/krekerok/springbootjwtdemo/services/UserServiceImpl.java
+++ src/main/java/com/krekerok/springbootjwtdemo/services/UserServiceImpl.java
@@ -10,6 +10,7 @@ import com.krekerok.springbootjwtdemo.pojo.MessageResponse;
 import com.krekerok.springbootjwtdemo.pojo.SignupRequest;
 import com.krekerok.springbootjwtdemo.repositories.RoleRepository;
 import com.krekerok.springbootjwtdemo.repositories.UserRepository;
+import com.krekerok.springbootjwtdemo.services.interfaces.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -25,7 +26,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
     private UserRepository userRespository;
     private RoleRepository roleRepository;
@@ -42,99 +43,77 @@ public class UserServiceImpl implements UserService{
         this.jwtUtils = jwtUtils;
     }
 
-
-
     @Override
     public ResponseEntity<?> authUser(LoginRequest loginRequest) {
-        Authentication authentication = authenticationManager
-                .authenticate(new UsernamePasswordAuthenticationToken(
-                        loginRequest.getUsername(),
-                        loginRequest.getPassword()));
 
+        Authentication authentication = getAuthentication(loginRequest);
         SecurityContextHolder.getContext().setAuthentication(authentication);
-
         String jwt = jwtUtils.generateJwtToken(authentication);
-
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
 
-        List<String> roles = userDetails.getAuthorities().stream()
-                .map(item -> item.getAuthority())
-                .collect(Collectors.toList());
+        List<String> roles = getRolesList(userDetails);
 
-
-        return ResponseEntity.ok(new JwtResponse(jwt, userDetails.getId(),
-                userDetails.getUsername(),
-                userDetails.getEmail(),
-                roles));
+        return ResponseEntity.ok(new JwtResponse(jwt, userDetails.getId(), userDetails.getUsername(), userDetails.getEmail(), roles));
     }
 
     @Override
     public ResponseEntity<?> registerUser(SignupRequest signupRequest) {
-        if (userRespository.existsByUsername(signupRequest.getUsername())) {
-            return ResponseEntity
-                    .badRequest()
-                    .body(new MessageResponse("Error: Username is exist"));
-        }
+        if (userRespository.existsByUsername(signupRequest.getUsername()))
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Username is exist"));
 
-        if (userRespository.existsByEmail(signupRequest.getEmail())) {
-            return ResponseEntity
-                    .badRequest()
-                    .body(new MessageResponse("Error: Email is exist"));
-        }
+        if (userRespository.existsByEmail(signupRequest.getEmail()))
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Email is exist"));
 
-        User user = new User(signupRequest.getUsername(),
-                signupRequest.getEmail(),
-                passwordEncoder.encode(signupRequest.getPassword()));
+        Set<Role> roles = fillRolesListWithDataFromRequestRolesList(signupRequest.getRoles());
+        createAndSaveUser(signupRequest, roles);
 
-        Set<String> reqRoles = signupRequest.getRoles();
+        return ResponseEntity.ok(new MessageResponse("User CREATED"));
+    }
+
+
+
+    private Authentication getAuthentication(LoginRequest loginRequest) {
+        return authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+    }
+
+    private List<String> getRolesList(UserDetailsImpl userDetails) {
+        return userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+    }
+
+
+    private void createAndSaveUser(SignupRequest signupRequest, Set<Role> roles) {
+        User user = new User(signupRequest.getUsername(), signupRequest.getEmail(), passwordEncoder.encode(signupRequest.getPassword()));
+        user.setRoles(roles);
+        userRespository.save(user);
+    }
+
+    private Set<Role> fillRolesListWithDataFromRequestRolesList(Set<String> reqRoles){
         Set<Role> roles = new HashSet<>();
-
-
         if (reqRoles == null) {
-
-            Role userRole = roleRepository
-                    .findByName(ERole.ROLE_USER)
-                    .orElseThrow(() -> new RuntimeException("Error, Role USER is not found"));
-            roles.add(userRole);
+            roles.add(getRoleFromEnum(ERole.ROLE_USER));
         } else {
-
-
             reqRoles.forEach(r -> {
                 switch (r) {
                     case "admin":
-
-
-                        Role adminRole = roleRepository
-                                .findByName(ERole.ROLE_ADMIN)
-                                .orElseThrow(() -> new RuntimeException("Error, Role ADMIN is not found"));
-                        roles.add(adminRole);
-
+                        roles.add(getRoleFromEnum(ERole.ROLE_ADMIN));
                         break;
                     case "mod":
-
-
-                        Role modRole = roleRepository
-                                .findByName(ERole.ROLE_MODERATOR)
-                                .orElseThrow(() -> new RuntimeException("Error, Role MODERATOR is not found"));
-                        roles.add(modRole);
-
+                        roles.add(getRoleFromEnum(ERole.ROLE_MODERATOR));
                         break;
-
                     default:
-
-
-                        Role userRole = roleRepository
-                                .findByName(ERole.ROLE_USER)
-                                .orElseThrow(() -> new RuntimeException("Error, Role USER is not found"));
-                        roles.add(userRole);
+                        roles.add(getRoleFromEnum(ERole.ROLE_USER));
                 }
             });
         }
+        return roles;
+    }
 
-
-        user.setRoles(roles);
-        userRespository.save(user);
-
-        return ResponseEntity.ok(new MessageResponse("User CREATED"));
+    private Role getRoleFromEnum(ERole eRole){
+        return roleRepository
+                .findByName(eRole)
+                .orElseThrow(() -> new RuntimeException(String.format("Error, Role %s is not found",  eRole.name())));
     }
 }
